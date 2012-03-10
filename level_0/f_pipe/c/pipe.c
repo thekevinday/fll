dddeb62d268245bc2ff7911b94be917a5456397e
Kevin Day <kevin@kevux.org>
2012-03-10 14:59:38 -0600

    Add Project: pipe
    This adds the level 0 implementation of project pipe.
    At this time, all it does is provide a way to tell if the current instance of some program has piped data being passed to it.

---
+++ level_0/f_pipe/c/pipe.c
@@ -0,0 +1,35 @@
+/* FLL - Level 0
+ * Project:       Pipe
+ * Version:       0.3.x
+ * Licenses:      lgplv2.1
+ * Programmers:   Kevin Day
+ * Documentation:
+ *
+ * Provides pipe functionality.
+ */
+#include <level_0/pipe.h>
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#ifndef _di_f_pipe_exists_
+  // returns f_true if the standard input contains piped data.
+  f_return_status f_pipe_exists(){
+    struct stat st_info;
+
+    if (fstat(fileno(f_pipe), &st_info) != 0) {
+      return f_file_stat_error;
+    }
+
+    if (S_ISFIFO(st_info.st_mode)) {
+      return f_true;
+    }
+
+    return f_false;
+  }
+#endif // _di_f_pipe_exists_
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
---
+++ level_0/f_pipe/c/pipe.h
@@ -0,0 +1,39 @@
+/* FLL - Level 0
+ * Project:       Pipe
+ * Version:       0.3.x
+ * Licenses:      lgplv2.1
+ * Programmers:   Kevin Day
+ * Documentation:
+ *
+ * Provides pipe functionality.
+ *
+ */
+#ifndef _F_pipe_h
+#define _F_pipe_h
+
+// libc includes
+#include <stdio.h>
+#include <sys/stat.h>
+
+// fll includes
+#include <level_0/errors.h>
+#include <level_0/types.h>
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#ifndef _di_f_pipe_
+  #define f_pipe f_standard_input
+#endif // _di_f_pipe_
+
+#ifndef _di_f_pipe_exists_
+  // returns f_true if the standard input contains piped data.
+  extern f_return_status f_pipe_exists();
+#endif // _di_f_pipe_exists_
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif // _F_pipe_h
