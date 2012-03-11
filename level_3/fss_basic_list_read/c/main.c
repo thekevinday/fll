9e83cd794cf40e37d24a16f67e2fa65e0a87cff9
Kevin Day <kevin@kevux.org>
2012-03-10 23:31:03 -0600

    Update: return the literal error code
    The error code was being converted into 0 if the code was f_none.
    Stop doing this behavior, all the scripts need to be fll return state aware from this point forward.

--- level_3/fss_basic_list_read/c/main.c
+++ level_3/fss_basic_list_read/c/main.c
@@ -1,16 +1,11 @@
 #include <level_3/fss_basic_list_read.h>
 
 int main(const f_array_length argc, const f_string argv[]){
-  f_status                 status = f_status_initialize;
-  fss_basic_list_read_data data   = fss_basic_list_read_data_initialize;
+  fss_basic_list_read_data data = fss_basic_list_read_data_initialize;
 
   if (f_pipe_exists()){
     data.process_pipe = f_true;
   }
 
-  status = fss_basic_list_read_main(argc, argv, &data);
-
-  if (status == f_none) return 0;
-
-  return status;
+  return fss_basic_list_read_main(argc, argv, &data);
 }
--- level_3/fss_extended_read/c/main.c
+++ level_3/fss_extended_read/c/main.c
@@ -1,16 +1,11 @@
 #include <level_3/fss_extended_read.h>
 
 int main(const f_array_length argc, const f_string argv[]){
-  f_status               status = f_status_initialize;
-  fss_extended_read_data data   = fss_extended_read_data_initialize;
+  fss_extended_read_data data = fss_extended_read_data_initialize;
 
   if (f_pipe_exists()){
     data.process_pipe = f_true;
   }
 
-  status = fss_extended_read_main(argc, argv, &data);
-
-  if (status == f_none) return 0;
-
-  return status;
+  return fss_extended_read_main(argc, argv, &data);
 }
