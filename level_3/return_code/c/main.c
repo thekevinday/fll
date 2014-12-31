fcdebdee58ab17a5b7915c2b278b546f284a8db8
Kevin Day <thekevinday@gmail.com>
2014-12-30 19:57:34 -0600

    Update: uncomment pipe detection code in return_code and fss_return_code
    Not sure why this was disabled, perhaps it was not implemented?
    At least, enable the variable.

--- level_3/return_code/c/main.c
+++ level_3/return_code/c/main.c
@@ -3,11 +3,9 @@
 int main(const f_array_length argc, const f_string argv[]) {
   return_code_data data = return_code_data_initialize;
 
-  /*
   if (f_pipe_exists()) {
     data.process_pipe = f_true;
   }
-  */
 
   return return_code_main(argc, argv, &data);
 }
