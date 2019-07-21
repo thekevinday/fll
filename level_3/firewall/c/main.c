cd727286b1604b80a11df017b142f3250a4ac3b2
Kevin Day <thekevinday@gmail.com>
2019-07-21 16:06:13 -0500

    Update: fix firewall utility and utilize recently added functionality
    There were a few logic flaws in regards to the processing and memory management.
    Switch to new logic where f_new_* and f_clear_* is utilized.
    Update accodingly to the fll_execute changes.
    
    Update the default configuration files.
    
    Update the debugging code to work as expected.
    
    Replace string cuntions with memory functions, such as replacing strncat with memcpy.
    
    Add some new helper macros.
    
    Delete all custom chains when rewriting rules to prevent problems.
    Iptables unfortunately does not provide a way to check and see if a chain already exists.

--- level_3/firewall/c/main.c
+++ level_3/firewall/c/main.c
@@ -1,7 +1,12 @@
+#include <level_0/errors.h>
 #include <level_3/firewall.h>
 
 int main(f_const f_s_int argc, f_const f_string argv[]) {
   firewall_data data = firewall_data_initialize;
 
-  return firewall_main(argc, argv, &data);
+  if (f_error_is_error(firewall_main(argc, argv, &data))) {
+    return 1;
+  }
+
+  return 0;
 }
