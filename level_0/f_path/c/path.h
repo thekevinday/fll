a0564f86ff12b8e0f7d519690593263192a0e175
Kevin Day <thekevinday@gmail.com>
2020-05-11 22:14:20 -0500

    Update: add path.h headerr

---
+++ level_0/f_path/c/path.h
@@ -0,0 +1,32 @@
+/**
+ * FLL - Level 0
+ *
+ * Project: Path
+ * API Version: 0.5
+ * Licenses: lgplv2.1
+ *
+ * Provide path-related functionality.
+ */
+#ifndef _F_path_h
+#define _F_path_h
+
+// fll-0 includes
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Standard path defines.
+ *
+ * The path separator must be a single 1-byte wide string.
+ */
+#ifndef _di_f_path_defines_
+  #define f_path_separator "/"
+#endif // _di_f_path_defines_
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif // _F_path_h
