88642d50923e08b38d66a9019ba1b88a0a4c1ed3
Kevin Day <thekevinday@gmail.com>
2020-11-04 20:22:19 -0600

    Update: Re-create Init program with barebones source structure.
    The Init program will instead be developed as an init-only program.
    The goal will be to not implement any service management.
    This means that another progrram will be written for that purpose (which will be called something like "control" and/or "controller").

--- level_3/init/c/init.c
+++ level_3/init/c/init.c
@@ -1,12 +1,3 @@
-/**
- * FLL - Level 3
- *
- * Project: Init
- * API Version: 0.5
- * Licenses: lgplv2.1
- *
- * This is the FLL Init program.
- */
 #include "init.h"
 #include "private-init.h"
 
@@ -29,11 +20,6 @@ extern "C" {
     fll_program_print_help_option(output, context, f_console_standard_short_debug, f_console_standard_long_debug, f_console_symbol_short_disable, f_console_symbol_long_disable, "   Enable debugging, inceasing verbosity beyond normal output.");
     fll_program_print_help_option(output, context, f_console_standard_short_version, f_console_standard_long_version, f_console_symbol_short_disable, f_console_symbol_long_disable, " Print only the version number.");
 
-    fprintf(output.stream, "%c", f_string_eol[0]);
-
-    fll_program_print_help_option(output, context, init_parameter_no_prepare_short_name, init_parameter_no_prepare_long_name, f_console_symbol_short_enable, f_console_symbol_long_enable, " Do not attempt to process kernel command line or perform any boot-time specific preparations.");
-    fll_program_print_help_option(output, context, init_parameter_runlevel_short_name, init_parameter_runlevel_long_name, f_console_symbol_short_enable, f_console_symbol_long_enable, " Specify a custom run level, ignoring the kernel command line runlevel argument.");
-
     fll_program_print_help_usage(output, context, init_name, "");
 
     return F_none;
@@ -42,13 +28,7 @@ extern "C" {
 
 #ifndef _di_init_main_
   f_return_status init_main(const f_console_arguments_t arguments, init_data_t *data) {
-    f_status_t status  = F_none;
-    int8_t run_level[init_kernel_runlevel_buffer];
-
-    memset(run_level, 0, init_kernel_runlevel_buffer);
-
-    unsigned short do_socket_file = F_true;
-    unsigned short do_socket_port = F_false;
+    f_status_t status = F_none;
 
     {
       const f_console_parameters_t parameters = f_macro_console_parameters_t_initialize(data->parameters, init_total_parameters);
@@ -63,7 +43,12 @@ extern "C" {
         data->error.notable = data->context.set.notable;
 
         if (F_status_is_error(status)) {
-          // @todo: init_delete_data(data);
+          if (data->error.verbosity != f_console_verbosity_quiet) {
+            fll_error_print(data->error, F_status_set_fine(status), "fll_program_parameter_process", F_true);
+            fprintf(data->error.to.stream, "%c", f_string_eol[0]);
+          }
+
+          init_delete_data(data);
           return F_status_set_error(status);
         }
       }
@@ -112,138 +97,36 @@ extern "C" {
       return F_none;
     }
 
-    if (data->parameters[init_parameter_runlevel].result == f_console_result_found) {
-      const unsigned int parameter_length = strlen(arguments.argv[data->parameters[init_parameter_runlevel].values.array[0]]);
+    // @todo
 
-      // if the run_level value is greater than the static buffer size, ignore the entire string rather than process a cut off value.
-      if (parameter_length > 0 && parameter_length < init_kernel_runlevel_buffer) {
-        strncpy((char *) &run_level, arguments.argv[data->parameters[init_parameter_runlevel].values.array[0]], parameter_length);
+    // ensure a newline is always put at the end of the program execution, unless in quiet mode.
+    if (data->error.verbosity != f_console_verbosity_quiet) {
+      if (F_status_is_error(status)) {
+        fprintf(data->error.to.stream, "%c", f_string_eol[0]);
       }
     }
 
-
-    // before doing anything make sure that the rootfs has been properly setup.
-    if (data->parameters[init_parameter_no_prepare].result == f_console_result_none) {
-      //init_prepare_system(&run_level);
-    }
-
-    // make sure appropriate required directories exist.
-    //init_prepare_init();
-
-
-    // attempt to load the main rule before forking and starting child processes.
-    {
-      // @todo: should this pre-scan the main rule file to see if there is a custom chroot command, if so, change behavior to process rules and then chroot into a new system (cleaning up resources as much as possible before doing chroot) (don't fork child processes?)?
-      // 1) check the last object for a chroot command in reverse order.
-      //   - if the chroot fails then additional rules may apply. the chroot command does not have to be the last command.
-      // 2) if chroot is not specified, then continue onto code below.
-      // 3) if chroot is specified, then execute rules directly and exit this process on success (via chroot command).
-      // 4) if chroot is specified, but chroot fails, then continue processing rules and then exit this process (dropping to bash shell).
-      // effectively use my initrd_init and pre_init code here.
-
-      // @todo: init_process_main_rule();
-    }
-
-
-    // commented out code is an alternative method to avoid using malloc for stacks passed to clone.
-    //f_char stack_services[init_stack_size_small_services];
-    //f_char stack_control_file[init_stack_size_small_control_file];
-    init_stack_memory stack_memory = init_stack_memory_initialize;
-
-    //status = init_initialize_stack_memory(&stack_memory);
-    if (F_status_is_error(status)) {
-      // @todo: init_delete_data((*data));
-      // @todo: init_delete_stack_memory(&stack_memory);
-      return status;
-    }
-
-    {
-      /*
-      pid_t pid_services = clone(init_handler_child_services, stack_memory.services + init_stack_size_small_services, init_flags_clone, stack_memory.services);
-
-      if (pid_services < 0) {
-        fl_color_print(data->error.to.stream, data->context.set.error, "%sFailed to clone services process (errno = %i).%c", fll_error_print_error, errno, f_string_eol[0]);
-      }
-
-      pid_t pid_control_file = clone(init_handler_child_control_file, stack_memory.control_file + init_stack_size_control_file, init_flags_clone, stack_memory.control_file);
-
-      if (pid_control_file < 0) {
-        fl_color_print(data->error.to.stream, data->context.set.error, "%sFailed to clone control via file process (errno = %i).%c", fll_error_print_error, errno, f_string_eol[0]);
-      }
-    */
-
-      // block signals
-      sigset_t  signal_mask;
-      siginfo_t signal_information;
-
-      memset(&signal_mask, 0, sizeof(sigset_t));
-      memset(&signal_information, 0, sizeof(siginfo_t));
-
-      // block all signals.
-      sigfillset(&signal_mask);
-      sigprocmask(SIG_BLOCK, &signal_mask, 0);
-
-      int signal_problem_count = 0;
-      const int problem_count_max_signal_size = 10;
-
-      // sit and wait for signals.
-      for (;;) {
-        int signal_result = sigwaitinfo(&signal_mask, &signal_information);
-
-        if (signal_result < 0) {
-          if (errno == EAGAIN) {
-            // do nothing.
-            continue;
-          }
-          else if (errno != EINTR) {
-            fl_color_print(data->error.to.stream, data->context.set.error, "%ssigwaitinfo() failed (errno = %i).%c", fll_error_print_error, errno, f_string_eol[0]);
-
-            signal_problem_count++;
-            if (signal_problem_count > problem_count_max_signal_size) {
-              fl_color_print(data->error.to.stream, data->context.set.error, "%sMax signal problem count has been reached, sleeping for a period of time.%c", fll_error_print_error, errno, f_string_eol[0]);
-              sleep(init_panic_signal_sleep_seconds);
-              signal_problem_count = 0;
-            }
-
-            continue;
-          }
-        }
-
-        signal_problem_count = 0;
-
-        if (signal_information.si_signo == SIGHUP) {
-          // @todo: close all child process connections? try using small initial memory instead?
-        }
-        else if (signal_information.si_signo == SIGINT) {
-          // check the status of processes to see if they are still running, if not, then restart them.
-        }
-        else if (signal_information.si_signo == SIGQUIT || signal_information.si_signo == SIGTERM) {
-          // @todo: block these or attempt to respawn init process? try using small initial memory instead?
-          break;
-        }
-        else if (signal_information.si_signo == SIGSEGV || signal_information.si_signo == SIGBUS || signal_information.si_signo == SIGILL || signal_information.si_signo == SIGFPE) {
-          // @todo: block these or attempt to respawn init process?
-        }
-        else if (signal_information.si_signo == SIGPWR) {
-          // @todo: shutdown process?
-        }
-        else if (signal_information.si_signo == SIGABRT || signal_information.si_signo == SIGIOT || signal_information.si_signo == SIGXCPU) {
-          // do nothing.
-        }
-        else if (signal_information.si_signo == SIGCHLD) {
-          // @todo: restart child processes? try using large initial memory instead?
-        }
-
-        memset(&signal_information, 0, sizeof(siginfo_t));
-        continue;
-      } // for
-    }
-
-    // @todo: init_delete_data((*data));
-    // @todo: init_delete_stack_memory(&stack_memory);
+    init_delete_data(data);
     return status;
   }
 #endif // _di_init_main_
+
+#ifndef _di_init_delete_data_
+  f_return_status init_delete_data(init_data_t *data) {
+
+    for (f_string_length_t i = 0; i < init_total_parameters; i++) {
+      f_macro_string_lengths_t_delete_simple(data->parameters[i].locations);
+      f_macro_string_lengths_t_delete_simple(data->parameters[i].locations_sub);
+      f_macro_string_lengths_t_delete_simple(data->parameters[i].values);
+    } // for
+
+    f_macro_string_lengths_t_delete_simple(data->remaining);
+
+    f_macro_color_context_t_delete_simple(data->context);
+
+    return F_none;
+  }
+#endif // _di_init_delete_data_
 
 #ifdef __cplusplus
 } // extern "C"
--- level_3/init/c/init.h
+++ level_3/init/c/init.h
@@ -1,62 +1,24 @@
-/* FLL - Level 3
- * Project:       Firewall
- * Version:       0.1.0
- * Licenses:      lgplv2.1
- * Programmers:   Kevin Day
- * Documentation:
+/**
+ * FLL - Level 3
  *
- * This is the Kevux Operating System Init program.
+ * Project: Init
+ * API Version: 0.5
+ * Licenses: lgplv2.1
+ *
+ * This is the Init program.
+ *
  * This program utilizes the Featureless Linux Library.
+ * This program performs the basic system boot operations.
  *
- * This init program is designed to run with certain paths and if they do not exist, it will attempt to create them.
- * This expects the Kevux path permissions structure and naming, so a valid /etc/group file should exist.
- *
- * This is designed to have different threads handling the following actions:
- *   Thread Group 1 (Signals and Control)
- *   - (parent process) handles/blocks all signals for init program and handles children accordingly.
- *   - runs as root.
- *
- *   Thread Group 2 (Services)
- *   - Handles starting and stopping of individual services.
- *   - New thread created for each user.
- *   - Runs as root.
- *   - Each individual thread runs as a specified user.
- *
- *   Thread Group 3 (Reporting and Output)
- *   - Handles output to the terminal or any other supported devices.
- *   - No child threads.
- *   - Runs as init user.
- *
- *   Thread Group 4 (Commands: Socket File)
- *   - If socket file is enabled, then this thread is created to listen on the socket file and process received commands.
- *   - No child threads.
- *   - Runs as init user.
- *
- *   Thread Group 5 (Commands: Socket Port)
- *   - If socket port is enabled, then this thread is created to listen on the socket port and process received commands.
- *   - No child threads.
- *   - Runs as init user.
- *
- * It would be nice to be able to start the init program as a non-root user who has elevated privileges similar to root.
- * - That is, have ability to su to any user (except root).
- * - While this will still be a security concern, not running as root will significantly reduce the potential problems.
- *   - This design would not allow for root-specific cron jobs.
+ * This program does the following:
+ * 1) perform all initial filesystem setup tasks (creating directories, mounting, setting permissions, populated device files, etc..).
+ * 2) finding the desired/wanted boot partition, mounting it (and any other mount points).
+ * 3) booting into target partition using a given target program (controller, bash, etc..).
+ * 4) falling back into a failsafe environment on error (such as bash).
  */
 #ifndef _init_h
 
-#define _GNU_SOURCE
-
 // libc includes
-#include <sched.h>
-#include <stdio.h>
-#include <string.h>
-#include <dirent.h>
-#include <unistd.h>
-#include <sys/mount.h>
-#include <sys/types.h>
-#include <regex.h>
-#include <malloc.h>
-#include <stdlib.h>
 
 // fll-0 includes
 #include <level_0/type.h>
@@ -67,23 +29,16 @@
 #include <level_0/console.h>
 #include <level_0/directory.h>
 #include <level_0/file.h>
-#include <level_0/fss.h>
 #include <level_0/pipe.h>
 #include <level_0/print.h>
 
 // fll-1 includes
 #include <level_1/color.h>
 #include <level_1/console.h>
-#include <level_1/fss.h>
-#include <level_1/fss_basic_list.h>
-#include <level_1/fss_extended.h>
 #include <level_1/string.h>
 
 // fll-2 includes
 #include <level_2/error.h>
-#include <level_2/execute.h>
-#include <level_2/fss_basic_list.h>
-#include <level_2/fss_extended.h>
 #include <level_2/program.h>
 
 #ifdef __cplusplus
@@ -93,86 +48,16 @@ extern "C" {
 #ifndef _di_init_version_
   #define init_major_version "0"
   #define init_minor_version "5"
-  #define init_micro_version "1"
+  #define init_micro_version "2"
   #define init_version init_major_version "." init_minor_version "." init_micro_version
 #endif // _di_init_version_
 
 #ifndef _di_init_name_
   #define init_name "init"
-  #define init_name_long "Kevux Init Program"
+  #define init_name_long "Init Program"
 #endif // _di_init_name_
 
-#ifndef _di_init_default_allocation_step_
-  #define init_default_allocation_step f_memory_default_allocation_step
-#endif // _di_init_default_allocation_step_
-
-#ifndef _di_init_paths_
-  // @todo: this will eventually be written to use the f_path project.
-  #define init_paths_rootfs         "/"
-  #define init_paths_devices        "/dev/"
-  #define init_paths_devices_pts    "/dev/pts/"
-  #define init_paths_system         "/sys/"
-  #define init_paths_processes      "/proc/"
-  #define init_paths_temporary      "/tmp/"
-  #define init_paths_mount          "/mnt/"
-  #define init_paths_run            "/run/"
-  #define init_paths_init_run       "/run/init/"
-  #define init_paths_init_settings  "/etc/init/"
-  #define init_paths_init_socket    "/run/init/socket/"
-  #define init_paths_init_process   "/run/init/process/"
-  #define init_paths_init_log       "/log/init/"
-  #define init_paths_init_log       "/tmp/init/"
-  #define init_paths_log            "/log/"
-#endif // _di_init_paths_
-
-#ifndef _di_init_extension_
-  #define init_extension_settings  "settings"
-  #define init_extension_rule      "rule"
-  #define init_extension_socket    "socket"
-  #define init_extension_process   "pid"
-#endif // _di_init_extension_
-
-#ifndef _di_init_settings_
-  #define init_rule_core       "main"
-  #define init_rule_core_file  init_paths_init_settings init_rule_core "." init_extension_rule
-
-  #define init_kernel_command_line     init_paths_processes "cmdline"
-  #define init_kernel_runlevel         "\\<runlevel=[[:alnum:][:punct:]]*"
-  #define init_kernel_runlevel_ignore  9
-  #define init_kernel_runlevel_buffer  256
-#endif // _di_init_settings_
-
-#ifndef _di_init_socket_
-  #define init_socket_control  "control"
-#endif // _di_init_settings_
-
-#ifndef _di_init_groups_
-  #define init_group_devices          "d_device"
-  #define init_group_devices_pts      "d_devpts"
-  #define init_group_system           "k_system"
-  #define init_group_processes        "k_process"
-  #define init_group_temporary        "d_temporary"
-  #define init_group_mount            "d_mount"
-  #define init_group_run              "d_run"
-  #define init_group_init_run         "d_init_run"
-  #define init_group_init_settings    "d_init_settings"
-  #define init_group_init_socket      "d_init_socket"
-  #define init_group_init_process     "d_init_process"
-  #define init_group_init_log         "d_log"
-  #define init_group_init_tmp         "d_init"
-  #define init_group_process_null     "p_null"
-  #define init_group_process_zero     "p_zero"
-  #define init_group_process_console  "p_terminal"
-  #define init_group_process_random   "p_random"
-  #define init_group_process_urandom  "p_random"
-#endif // _di_init_groups_
-
 #ifndef _di_init_defines_
-  #define init_parameter_runlevel_short_name    "r"
-  #define init_parameter_no_prepare_short_name  "n"
-
-  #define init_parameter_runlevel_long_name    "run_level"
-  #define init_parameter_no_prepare_long_name  "no_prepare"
 
   enum {
     init_parameter_help,
@@ -182,51 +67,24 @@ extern "C" {
     init_parameter_verbosity_quiet,
     init_parameter_verbosity_normal,
     init_parameter_verbosity_verbose,
-
-    #ifdef _en_init_debug_
-      init_parameter_verbosity_debug,
-    #endif // _en_init_debug_
-
+    init_parameter_verbosity_debug,
     init_parameter_version,
-
-    init_parameter_runlevel,
-    init_parameter_no_prepare,
   };
 
-  #ifdef _en_init_debug_
-    #define init_console_parameter_t_initialize \
-      { \
-        f_console_parameter_t_initialize(f_console_standard_short_help, f_console_standard_long_help, 0, 0, f_console_type_normal), \
-        f_console_parameter_t_initialize(f_console_standard_short_light, f_console_standard_long_light, 0, 0, f_console_type_inverse), \
-        f_console_parameter_t_initialize(f_console_standard_short_dark, f_console_standard_long_dark, 0, 0, f_console_type_inverse), \
-        f_console_parameter_t_initialize(f_console_standard_short_no_color, f_console_standard_long_no_color, 0, 0, f_console_type_inverse), \
-        f_console_parameter_t_initialize(f_console_standard_short_quiet, f_console_standard_long_quiet, 0, 0, f_console_type_inverse), \
-        f_console_parameter_t_initialize(f_console_standard_short_normal, f_console_standard_long_normal, 0, 0, f_console_type_inverse), \
-        f_console_parameter_t_initialize(f_console_standard_short_verbose, f_console_standard_long_verbose, 0, 0, f_console_type_inverse), \
-        f_console_parameter_t_initialize(f_console_standard_short_debug, f_console_standard_long_debug, 0, 0, f_console_type_inverse), \
-        f_console_parameter_t_initialize(f_console_standard_short_version, f_console_standard_long_version, 0, 0, f_console_type_inverse), \
-        f_console_parameter_t_initialize(init_parameter_runlevel_short_name, init_parameter_runlevel_long_name, 0, 1, f_console_type_normal), \
-        f_console_parameter_t_initialize(init_parameter_no_prepare_short_name, init_parameter_no_prepare_long_name, 0, 1, f_console_type_normal), \
-      }
+  #define init_console_parameter_t_initialize \
+    { \
+      f_console_parameter_t_initialize(f_console_standard_short_help, f_console_standard_long_help, 0, 0, f_console_type_normal), \
+      f_console_parameter_t_initialize(f_console_standard_short_light, f_console_standard_long_light, 0, 0, f_console_type_inverse), \
+      f_console_parameter_t_initialize(f_console_standard_short_dark, f_console_standard_long_dark, 0, 0, f_console_type_inverse), \
+      f_console_parameter_t_initialize(f_console_standard_short_no_color, f_console_standard_long_no_color, 0, 0, f_console_type_inverse), \
+      f_console_parameter_t_initialize(f_console_standard_short_quiet, f_console_standard_long_quiet, 0, 0, f_console_type_inverse), \
+      f_console_parameter_t_initialize(f_console_standard_short_normal, f_console_standard_long_normal, 0, 0, f_console_type_inverse), \
+      f_console_parameter_t_initialize(f_console_standard_short_verbose, f_console_standard_long_verbose, 0, 0, f_console_type_inverse), \
+      f_console_parameter_t_initialize(f_console_standard_short_debug, f_console_standard_long_debug, 0, 0, f_console_type_inverse), \
+      f_console_parameter_t_initialize(f_console_standard_short_version, f_console_standard_long_version, 0, 0, f_console_type_inverse), \
+    }
 
-    #define init_total_parameters 11
-  #else
-    #define init_console_parameter_t_initialize \
-      { \
-        f_console_parameter_t_initialize(f_console_standard_short_help, f_console_standard_long_help, 0, 0, f_console_type_normal), \
-        f_console_parameter_t_initialize(f_console_standard_short_light, f_console_standard_long_light, 0, 0, f_console_type_inverse), \
-        f_console_parameter_t_initialize(f_console_standard_short_dark, f_console_standard_long_dark, 0, 0, f_console_type_inverse), \
-        f_console_parameter_t_initialize(f_console_standard_short_no_color, f_console_standard_long_no_color, 0, 0, f_console_type_inverse), \
-        f_console_parameter_t_initialize(f_console_standard_short_quiet, f_console_standard_long_quiet, 0, 0, f_console_type_inverse), \
-        f_console_parameter_t_initialize(f_console_standard_short_normal, f_console_standard_long_normal, 0, 0, f_console_type_inverse), \
-        f_console_parameter_t_initialize(f_console_standard_short_verbose, f_console_standard_long_verbose, 0, 0, f_console_type_inverse), \
-        f_console_parameter_t_initialize(f_console_standard_short_version, f_console_standard_long_version, 0, 0, f_console_type_inverse), \
-        f_console_parameter_t_initialize(init_parameter_runlevel_short_name, init_parameter_runlevel_long_name, 0, 1, f_console_type_normal), \
-        f_console_parameter_t_initialize(init_parameter_no_prepare_short_name, init_parameter_no_prepare_long_name, 0, 1, f_console_type_normal), \
-      }
-
-    #define init_total_parameters 10
-  #endif // _en_init_debug_
+  #define init_total_parameters 9
 #endif // _di_init_defines_
 
 #ifndef _di_init_data_t_
@@ -251,42 +109,6 @@ extern "C" {
       fll_error_print_t_initialize, \
       f_color_context_t_initialize, \
     }
-
-  typedef struct {
-    uint8_t todo_placeholder;
-  } init_data_thread_control_t;
-
-  #define init_data_thread_control_t_initialize \
-    { \
-      0 \
-    }
-
-  typedef struct {
-    uint8_t todo_placeholder;
-  } init_data_thread_service_t;
-
-  #define init_data_thread_service_t_initialize \
-    { \
-      0 \
-    }
-
-  typedef struct {
-    uint8_t todo_placeholder;
-  } init_data_thread_socket_file_t;
-
-  #define init_data_thread_socket_file_t_initialize \
-    { \
-      0 \
-    }
-
-  typedef struct {
-    uint8_t todo_placeholder;
-  } init_data_thread_socket_port_t;
-
-  #define init_data_thread_socket_port_t_initialize \
-    { \
-      0 \
-    }
 #endif // _di_init_data_t_
 
 /**
@@ -307,6 +129,8 @@ extern "C" {
 /**
  * Execute main program.
  *
+ * Be sure to call init_delete_data() after executing this.
+ *
  * @param arguments
  *   The parameters passed to the process.
  * @param data
@@ -316,16 +140,18 @@ extern "C" {
  *   F_none on success.
  *
  *   Status codes (with error bit) are returned on any problem.
+ *
+ * @see init_delete_data()
  */
 #ifndef _di_init_main_
   extern f_return_status init_main(const f_console_arguments_t arguments, init_data_t *data);
 #endif // _di_init_main_
 
 /**
- * Execute main program.
+ * Deallocate data.
  *
- * @param arguments
- *   The parameters passed to the process.
+ * Be sure to call this after executing init_main().
+ *
  * @param data
  *   The program data.
  *
@@ -333,10 +159,12 @@ extern "C" {
  *   F_none on success.
  *
  *   Status codes (with error bit) are returned on any problem.
+ *
+ * @see init_main()
  */
-#ifndef _di_init_main_
-  extern f_return_status init_thread_control(const init_data_t *data);
-#endif // _di_init_main_
+#ifndef _di_init_delete_data_
+  extern f_return_status init_delete_data(init_data_t *data);
+#endif // _di_init_delete_data_
 
 #ifdef __cplusplus
 } // extern "C"
