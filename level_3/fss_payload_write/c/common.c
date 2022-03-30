14cbf34f92a4f8c31a46a9dc59ba48e11036c620
Kevin Day <thekevinday@gmail.com>
2022-03-29 22:54:34 -0500

    Update: Move the program data into the main for fss_payload_write.
    Move parameter initialization and deallocation for FLL program data into main.c.

--- level_3/fss_payload_write/c/common.c
+++ level_3/fss_payload_write/c/common.c
@@ -32,13 +32,6 @@ extern "C" {
   const f_string_static_t fss_payload_write_long_trim_s = macro_f_string_static_t_initialize(FSS_PAYLOAD_WRITE_long_trim_s, 0, FSS_PAYLOAD_WRITE_long_trim_s_length);
 #endif // _di_fss_payload_write_parameters_
 
-#ifndef _di_fss_payload_write_main_delete_
-  f_status_t fss_payload_write_main_delete(fll_program_data_t * const main) {
-
-    return fll_program_data_delete(main);
-  }
-#endif // _di_fss_payload_write_main_delete_
-
 #ifdef __cplusplus
 } // extern "C"
 #endif
