ca2f3c1ce7ecbb311a02d81369da6aa5d7b291a5
Kevin Day <thekevinday@gmail.com>
2022-03-29 23:02:31 -0500

    Update: Move the program data into the main for fss_extended_write.
    Move parameter initialization and deallocation for FLL program data into main.c.

--- level_3/fss_extended_write/c/fss_extended_write.c
+++ level_3/fss_extended_write/c/fss_extended_write.c
@@ -61,10 +61,6 @@ extern "C" {
 
     f_status_t status = F_none;
 
-    f_console_parameter_t parameters[] = fss_extended_write_console_parameter_t_initialize;
-    main->parameters.array = parameters;
-    main->parameters.used = fss_extended_write_total_parameters_d;
-
     {
       f_console_parameter_id_t ids[3] = { fss_extended_write_parameter_no_color_e, fss_extended_write_parameter_light_e, fss_extended_write_parameter_dark_e };
       const f_console_parameter_ids_t choices = macro_f_console_parameter_ids_t_initialize(ids, 3);
@@ -91,10 +87,7 @@ extern "C" {
         fll_program_parameter_process_empty(&main->context, sets);
       }
 
-      if (F_status_is_error(status)) {
-        fss_extended_write_main_delete(main);
-        return F_status_set_error(status);
-      }
+      if (F_status_is_error(status)) return F_status_set_error(status);
     }
 
     // Identify priority of verbosity related parameters.
@@ -104,12 +97,7 @@ extern "C" {
       const f_console_parameter_ids_t choices = macro_f_console_parameter_ids_t_initialize(ids, 4);
 
       status = f_console_parameter_prioritize_right(main->parameters, choices, &choice);
-
-      if (F_status_is_error(status)) {
-        fss_extended_write_main_delete(main);
-
-        return status;
-      }
+      if (F_status_is_error(status)) return status;
 
       if (choice == fss_extended_write_parameter_verbosity_quiet_e) {
         main->output.verbosity = f_console_verbosity_quiet_e;
@@ -140,15 +128,11 @@ extern "C" {
     if (main->parameters.array[fss_extended_write_parameter_help_e].result == f_console_result_found_e) {
       fss_extended_write_print_help(main->output.to, main->context);
 
-      fss_extended_write_main_delete(main);
-
       return status;
     }
 
     if (main->parameters.array[fss_extended_write_parameter_version_e].result == f_console_result_found_e) {
       fll_program_print_version(main->output.to, fss_extended_write_program_version_s);
-
-      fss_extended_write_main_delete(main);
 
       return status;
     }
@@ -592,7 +576,6 @@ extern "C" {
 
     f_string_dynamic_resize(0, &buffer);
     f_string_dynamics_resize(0, &contents);
-    fss_extended_write_main_delete(main);
 
     return status;
   }
