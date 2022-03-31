7a2e307546893ac661ced169a83dae2962091d22
Kevin Day <thekevinday@gmail.com>
2022-03-30 19:14:55 -0500

    Update: Move the program data into the main for fss_basic_list_read.
    Move parameter initialization and deallocation for FLL program data into main.c.

--- level_3/fss_basic_list_read/c/common.c
+++ level_3/fss_basic_list_read/c/common.c
@@ -61,13 +61,6 @@ extern "C" {
   const f_string_static_t fss_basic_list_read_delimit_mode_name_lesser_s = macro_f_string_static_t_initialize(FSS_BASIC_LIST_READ_delimit_mode_name_lesser_s, 0, FSS_BASIC_LIST_READ_delimit_mode_name_lesser_s_length);
 #endif // _di_fss_basic_list_read_delimit_mode_
 
-#ifndef _di_fss_basic_list_read_main_delete_
-  f_status_t fss_basic_list_read_main_delete(fll_program_data_t * const main) {
-
-    return fll_program_data_delete(main);
-  }
-#endif // _di_fss_basic_list_read_main_delete_
-
 #ifdef __cplusplus
 } // extern "C"
 #endif
