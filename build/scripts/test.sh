5d92c461bf37e39d4d422bb2ef2ccadcdd3385cc
Kevin Day <kevin@kevux.org>
2023-03-29 18:30:15 -0500

    Regression: Build test.sh script is not properly running.
    Two mistakes are introduced by the commit 8f5854676725879488eaa1b378dc5d02e8169ae5.
    1) The error handling return status checks are against empty string and not "0".
    2) Accidental copy and paste mistake where print variable is being used as if it were a function.
    
    Remove the failure=0 assignment.
    The default of failure="" is what is needed.
    
    The variable print_line_first is supposed to be test_print_first.

--- build/scripts/test.sh
+++ build/scripts/test.sh
@@ -70,8 +70,6 @@ test_main() {
   local projects="f_type f_status f_memory f_type_array f_string f_utf f_account f_capability f_color f_compare f_console f_control_group f_conversion f_directory f_environment f_execute f_file f_fss f_iki f_limit f_parse f_path f_pipe f_print f_rip f_serialize f_signal f_socket f_status_string f_thread fl_control_group fl_conversion fl_directory fl_environment fl_execute fl_fss fl_iki fl_path fl_print fl_signal fl_utf_file fll_control_group fll_error fll_execute fll_file fll_fss fll_fss_status_string fll_iki fll_print fll_program fll_status_string"
   local projects_no_tests="f_type"
 
-  let failure=0
-
   if [[ $# -gt 0 ]] ; then
     t=$#
 
@@ -248,7 +246,7 @@ test_main() {
 
     if [[ $? -ne 0 ]] ; then
       if [[ $verbosity != "quiet" ]] ; then
-        print_line_first
+        test_print_first
 
         echo -e "${c_error}ERROR: The test path ${c_notice}${path_test}${c_error}, does not exist and could not be created or exists and is not a valid directory.${c_reset}"
       fi
@@ -262,7 +260,7 @@ test_main() {
 
     if [[ $? -ne 0 ]] ; then
       if [[ $verbosity != "quiet" ]] ; then
-        print_line_first
+        test_print_first
 
         echo -e "${c_error}ERROR: The package path ${c_notice}${path_test_package}${c_error}, does not exist and could not be created or exists and is not a valid directory.${c_reset}"
       fi
@@ -276,7 +274,7 @@ test_main() {
 
     if [[ $? -ne 0 ]] ; then
       if [[ $verbosity != "quiet" ]] ; then
-        print_line_first
+        test_print_first
 
         echo -e "${c_error}ERROR: The test project path ${c_notice}${path_test_project}${c_error}, does not exist and could not be created or exists and is not a valid directory.${c_reset}"
       fi
@@ -290,7 +288,7 @@ test_main() {
 
     if [[ $? -ne 0 ]] ; then
       if [[ $verbosity != "quiet" ]] ; then
-        print_line_first
+        test_print_first
 
         echo -e "${c_error}ERROR: The test work path ${c_notice}${path_test_work}${c_error}, does not exist and could not be created or exists and is not a valid directory.${c_reset}"
       fi
