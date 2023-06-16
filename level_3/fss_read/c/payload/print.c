295ec461354633f47bb1025d02bcbc8f580013f9
Kevin Day <kevin@kevux.org>
2023-06-15 22:10:17 -0500

    Update: Finish the FSS Payload Read help message printing.
    I quickly reviewed the help messages and found that FSS Payload Read is the only one needing to be updated.

--- level_3/fss_read/c/payload/print.c
+++ level_3/fss_read/c/payload/print.c
@@ -35,19 +35,15 @@ extern "C" {
 
     fss_read_print_message_help_note(print, F_true);
 
-    // @todo use these stirngs in place of the format prints below.
-    //As an exceptional case, a --depth of 1 applies only to the explicit Object of 'header'.
-    //Content at this depth is processed as FSS-0001 Extended.
-    //
-    //The Content of the explicit Object of 'payload' will not contain any Content close pipe control codes when using --pipe.
+    fl_print_format("%r  As an exceptional case, a %[%r%r%] of", print->to, f_string_eol_s, print->set->notable, f_console_symbol_long_normal_s, fss_read_long_depth_s, print->set->notable);
+    fl_print_format(" %[1%] applies only to the explicit Object of", print->to, print->set->notable, print->set->notable);
+    fl_print_format(" '%[%r%]'.%r", print->to, print->set->notable, f_fss_header_s, print->set->notable, f_string_eol_s);
+    fl_print_format("  Content at this depth is processed as FSS-0001 (Extended).%r%r", print->to, f_string_eol_s, f_string_eol_s);
 
-    /*
-    fl_print_format("%r  The FSS-000E (Payload) specification does not support quoted names, therefore the parameters '%[%r%r%]'", print->to, f_string_eol_s, print->set->notable, f_console_symbol_long_normal_s, fss_read_long_single_s, print->set->notable);
-    fl_print_format(" and '%[%r%r%]' do nothing.%r%r", print->to, print->set->notable, f_console_symbol_long_normal_s, fss_read_long_double_s, print->set->notable, f_string_eol_s, f_string_eol_s);
-
-    fl_print_format("  This program does not use the parameter '%[%r%r%]', which therefore does nothing.%r", print->to, print->set->notable, f_console_symbol_long_normal_s, fss_read_long_ignore_s, print->set->notable, f_string_eol_s);
-    fl_print_format("  This parameter requires two values.%r", print->to, f_string_eol_s);
-    */
+    fl_print_format("  The Content of the explicit Object of", print->to);
+    fl_print_format(" '%[%r%]'", print->to, print->set->notable, f_fss_payload_s, print->set->notable, f_string_eol_s);
+    fl_print_format(" will not contain any Content close pipe control codes when using", print->to);
+    fl_print_format(" %[%r%r%].%r", print->to, print->set->notable, f_console_symbol_long_normal_s, fss_read_long_pipe_s, print->set->notable, f_string_eol_s);
 
     f_file_stream_flush(print->to);
     f_file_stream_unlock(print->to);
