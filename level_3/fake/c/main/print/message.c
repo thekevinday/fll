f4273d8320aec35291814d1b74a53cdbef6e1b43
Kevin Day <kevin@kevux.org>
2023-07-25 23:26:15 -0500

    Bugfix: Fix new lines when printing help.
    I noticed a couple of programs are missing the correct new lines when printing the help.

--- level_3/fake/c/main/print/message.c
+++ level_3/fake/c/main/print/message.c
@@ -99,7 +99,7 @@ extern "C" {
     fll_program_print_help_option(print, fake_short_sources_s, fake_long_sources_s, f_console_symbol_short_normal_s, f_console_symbol_long_normal_s, "  Specify a custom path to the source files.");
     fll_program_print_help_option(print, fake_short_work_s, fake_long_work_s, f_console_symbol_short_normal_s, f_console_symbol_long_normal_s, "     Use includes/libraries/programs from this directory instead of system.");
 
-    fl_print_format("%r%r %[Special Options:%]%r", print->to, f_string_eol_s, f_string_eol_s, print->set->important, print->set->important, f_string_eol_s);
+    fl_print_format("%r %[Special Options:%]%r", print->to, f_string_eol_s, print->set->important, print->set->important, f_string_eol_s);
 
     fll_program_print_help_option_long(print, fake_long_documents_disabled_s, f_console_symbol_long_normal_s, "   Forcibly do not build documents files.");
     fll_program_print_help_option_long(print, fake_long_documents_enabled_s, f_console_symbol_long_normal_s, "    Forcibly do build documents files.");
@@ -108,7 +108,7 @@ extern "C" {
     fll_program_print_help_option_long(print, fake_long_static_disabled_s, f_console_symbol_long_normal_s, "Forcibly do not build static files.");
     fll_program_print_help_option_long(print, fake_long_static_enabled_s, f_console_symbol_long_normal_s, " Forcibly do build static files.");
 
-    fl_print_format("%r %[Operations:%] ", print->to, f_string_eol_s, print->set->important, print->set->important);
+    fl_print_format("%r %[Operations:%] %r", print->to, f_string_eol_s, print->set->important, print->set->important, f_string_eol_s);
 
     fll_program_print_help_option_other(print, fake_other_operation_build_s, "   Build or compile the code based on build settings file.");
     fll_program_print_help_option_other(print, fake_other_operation_clean_s, "   Delete all build files.");
