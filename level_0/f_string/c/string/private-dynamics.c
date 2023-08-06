3533e7f840c5d852f98372f2dd9c9066cc53c85e
Kevin Day <kevin@kevux.org>
2023-08-05 21:54:46 -0500

    Regression: Follow up to invalid type used when allocating string, f_string_t should instead be f_char_t.
    I missed the save button on this file and this change did not get committed.

--- level_0/f_string/c/string/private-dynamics.c
+++ level_0/f_string/c/string/private-dynamics.c
@@ -76,7 +76,7 @@ extern "C" {
 
     for (f_number_unsigned_t i = length; i < structure->size; ++i) {
 
-      status = f_memory_array_resize(0, sizeof(f_string_t), (void **) &structure->array[i].string, &structure->array[i].used, &structure->array[i].size);
+      status = f_memory_array_resize(0, sizeof(f_char_t), (void **) &structure->array[i].string, &structure->array[i].used, &structure->array[i].size);
       if (F_status_is_error(status)) return status;
     } // for
 
