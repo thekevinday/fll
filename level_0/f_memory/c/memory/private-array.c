f2791392a30704dfb6d0cf019146a91d02e43a25
Kevin Day <kevin@kevux.org>
2023-08-05 19:36:16 -0500

    Cleanup: Surround constant variable in a scoped block.

--- level_0/f_memory/c/memory/private-array.c
+++ level_0/f_memory/c/memory/private-array.c
@@ -9,8 +9,10 @@ extern "C" {
 #if !defined(_di_f_memory_array_adjust_) || !defined(_di_f_memory_array_decimate_by_)
   f_status_t private_f_memory_array_adjust(const f_number_unsigned_t length, const size_t width, void ** array, f_number_unsigned_t * const used, f_number_unsigned_t * const size) {
 
-    const f_status_t status = private_f_memory_adjust(*size, length, width, array);
-    if (F_status_is_error(status)) return status;
+    {
+      const f_status_t status = private_f_memory_adjust(*size, length, width, array);
+      if (F_status_is_error(status)) return status;
+    }
 
     *size = length;
 
@@ -25,8 +27,10 @@ extern "C" {
 #if !defined(_di_f_memory_array_append_) || !defined(_di_f_memory_array_append_all_) || !defined(_di_f_memory_array_decrease_by_) || !defined(_di_f_memory_array_increase_) || !defined(_di_f_memory_array_increase_by_) || !defined(_di_f_memory_array_resize_)
   f_status_t private_f_memory_array_resize(const f_number_unsigned_t length, const size_t width, void ** array, f_number_unsigned_t * const used, f_number_unsigned_t * const size) {
 
-    const f_status_t status = private_f_memory_resize(*size, length, width, array);
-    if (F_status_is_error(status)) return status;
+    {
+      const f_status_t status = private_f_memory_resize(*size, length, width, array);
+      if (F_status_is_error(status)) return status;
+    }
 
     *size = length;
 
