7fcfe756d2997abcd9c2d208facdfbe70e2d6cbf
Kevin Day <thekevinday@gmail.com>
2023-11-10 23:35:42 -0600

    Progress: Continue adding FSS Payload processing code.
    Add the common, recommended, payload Object strings.

--- level_0/f_fss/c/fss/payload.c
+++ level_0/f_fss/c/fss/payload.c
@@ -25,9 +25,21 @@ extern "C" {
     const f_string_static_t f_fss_payload_object_header_s = macro_f_string_static_t_initialize_1(F_fss_payload_object_header_s, 0, F_fss_payload_object_header_s_length);
   #endif // _di_f_fss_payload_object_header_s_
 
+  #ifndef _di_f_fss_payload_object_id_s_
+    const f_string_static_t f_fss_payload_object_id_s = macro_f_string_static_t_initialize_1(F_fss_payload_object_id_s, 0, F_fss_payload_object_id_s_length);
+  #endif // _di_f_fss_payload_object_id_s_
+
   #ifndef _di_f_fss_payload_object_length_s_
     const f_string_static_t f_fss_payload_object_length_s = macro_f_string_static_t_initialize_1(F_fss_payload_object_length_s, 0, F_fss_payload_object_length_s_length);
   #endif // _di_f_fss_payload_object_length_s_
+
+  #ifndef _di_f_fss_payload_object_name_s_
+    const f_string_static_t f_fss_payload_object_name_s = macro_f_string_static_t_initialize_1(F_fss_payload_object_name_s, 0, F_fss_payload_object_name_s_length);
+  #endif // _di_f_fss_payload_object_name_s_
+
+  #ifndef _di_f_fss_payload_object_part_s_
+    const f_string_static_t f_fss_payload_object_part_s = macro_f_string_static_t_initialize_1(F_fss_payload_object_part_s, 0, F_fss_payload_object_part_s_length);
+  #endif // _di_f_fss_payload_object_part_s_
 
   #ifndef _di_f_fss_payload_object_payload_s_
     const f_string_static_t f_fss_payload_object_payload_s = macro_f_string_static_t_initialize_1(F_fss_payload_object_payload_s, 0, F_fss_payload_object_payload_s_length);
@@ -37,6 +49,10 @@ extern "C" {
     const f_string_static_t f_fss_payload_object_signature_s = macro_f_string_static_t_initialize_1(F_fss_payload_object_signature_s, 0, F_fss_payload_object_signature_s_length);
   #endif // _di_f_fss_payload_object_signature_s_
 
+  #ifndef _di_f_fss_payload_object_size_s_
+    const f_string_static_t f_fss_payload_object_size_s = macro_f_string_static_t_initialize_1(F_fss_payload_object_size_s, 0, F_fss_payload_object_size_s_length);
+  #endif // _di_f_fss_payload_object_size_s_
+
   #ifndef _di_f_fss_payload_object_status_s_
     const f_string_static_t f_fss_payload_object_status_s = macro_f_string_static_t_initialize_1(F_fss_payload_object_status_s, 0, F_fss_payload_object_status_s_length);
   #endif // _di_f_fss_payload_object_status_s_
@@ -44,6 +60,10 @@ extern "C" {
   #ifndef _di_f_fss_payload_object_total_s_
     const f_string_static_t f_fss_payload_object_total_s = macro_f_string_static_t_initialize_1(F_fss_payload_object_total_s, 0, F_fss_payload_object_total_s_length);
   #endif // _di_f_fss_payload_object_total_s_
+
+  #ifndef _di_f_fss_payload_object_type_s_
+    const f_string_static_t f_fss_payload_object_type_s = macro_f_string_static_t_initialize_1(F_fss_payload_object_type_s, 0, F_fss_payload_object_type_s_length);
+  #endif // _di_f_fss_payload_object_type_s_
 #endif // _di_f_fss_payload_s_
 
 #ifdef __cplusplus
--- level_0/f_fss/c/fss/payload.h
+++ level_0/f_fss/c/fss/payload.h
@@ -41,6 +41,8 @@ extern "C" {
 /**
  * FSS-000E (Payload) strings.
  *
+ * A small amount of recommended or assumed common "header" Content Object strings are provided and are noted as "an optional string".
+ *
  * f_fss_payload_comment_*_s:
  *   - header:       The complete header comment line for Payload (FSS-000E).
  *   - header_end:   The complete header comment line for Payload (FSS-000E).
@@ -49,12 +51,16 @@ extern "C" {
  * f_fss_payload_object_*_s:
  *   - end:       The Object end characters.
  *   - header:    The header Object or signature Content header Object.
- *   - length:    The header Content length Object.
- *   - part:      The header Content part Object.
+ *   - id:        The header Content id Object (an optional string).
+ *   - length:    The header Content length Object (an optional string).
+ *   - name:      The header Content name Object (an optional string).
+ *   - part:      The header Content part Object (an optional string).
  *   - payload:   The payload Object or signature Content payload Object.
  *   - signature: The signature Object.
- *   - status:    The header Content status Object.
- *   - total:     The header Content total Object.
+ *   - size:      The header Content size Object (an optional string).
+ *   - status:    The header Content status Object (an optional string).
+ *   - total:     The header Content total Object (an optional string).
+ *   - type:      The header Content type Object (an optional string).
  */
 #ifndef _di_f_fss_payload_s_
   #define F_fss_payload_comment_header_s       "fss-000e"
@@ -63,25 +69,33 @@ extern "C" {
 
   #define F_fss_payload_object_end_s       ":\n"
   #define F_fss_payload_object_header_s    "header"
+  #define F_fss_payload_object_id_s        "id"
   #define F_fss_payload_object_length_s    "length"
+  #define F_fss_payload_object_name_s      "name"
   #define F_fss_payload_object_part_s      "part"
   #define F_fss_payload_object_payload_s   "payload"
   #define F_fss_payload_object_signature_s "signature"
+  #define F_fss_payload_object_size_s      "size"
   #define F_fss_payload_object_status_s    "status"
   #define F_fss_payload_object_total_s     "total"
+  #define F_fss_payload_object_type_s      "type"
 
   #define F_fss_payload_comment_header_s_length       8
   #define F_fss_payload_comment_header_end_s_length   1
   #define F_fss_payload_comment_header_begin_s_length 2
 
   #define F_fss_payload_object_end_s_length       2
-  #define F_fss_payload_object_length_s_length    6
   #define F_fss_payload_object_header_s_length    6
+  #define F_fss_payload_object_id_s_length        2
+  #define F_fss_payload_object_length_s_length    6
+  #define F_fss_payload_object_name_s_length      4
   #define F_fss_payload_object_part_s_length      4
   #define F_fss_payload_object_payload_s_length   7
   #define F_fss_payload_object_signature_s_length 9
+  #define F_fss_payload_object_size_s_length      4
   #define F_fss_payload_object_status_s_length    6
   #define F_fss_payload_object_total_s_length     5
+  #define F_fss_payload_object_type_s_length      4
 
   #ifndef _di_f_fss_payload_comment_header_s_
     extern const f_string_static_t f_fss_payload_comment_header_s;
@@ -103,9 +117,21 @@ extern "C" {
     extern const f_string_static_t f_fss_payload_object_header_s;
   #endif // _di_f_fss_payload_object_header_s_
 
+  #ifndef _di_f_fss_payload_object_id_s_
+    extern const f_string_static_t f_fss_payload_object_id_s;
+  #endif // _di_f_fss_payload_object_id_s_
+
   #ifndef _di_f_fss_payload_object_length_s_
     extern const f_string_static_t f_fss_payload_object_length_s;
   #endif // _di_f_fss_payload_object_length_s_
+
+  #ifndef _di_f_fss_payload_object_name_s_
+    extern const f_string_static_t f_fss_payload_object_name_s;
+  #endif // _di_f_fss_payload_object_name_s_
+
+  #ifndef _di_f_fss_payload_object_part_s_
+    extern const f_string_static_t f_fss_payload_object_part_s;
+  #endif // _di_f_fss_payload_object_part_s_
 
   #ifndef _di_f_fss_payload_object_payload_s_
     extern const f_string_static_t f_fss_payload_object_payload_s;
@@ -115,9 +141,21 @@ extern "C" {
     extern const f_string_static_t f_fss_payload_object_signature_s;
   #endif // _di_f_fss_payload_object_signature_s_
 
+  #ifndef _di_f_fss_payload_object_size_s_
+    extern const f_string_static_t f_fss_payload_object_size_s;
+  #endif // _di_f_fss_payload_object_size_s_
+
   #ifndef _di_f_fss_payload_object_status_s_
     extern const f_string_static_t f_fss_payload_object_status_s;
   #endif // _di_f_fss_payload_object_status_s_
+
+  #ifndef _di_f_fss_payload_object_total_s_
+    extern const f_string_static_t f_fss_payload_object_total_s;
+  #endif // _di_f_fss_payload_object_total_s_
+
+  #ifndef _di_f_fss_payload_object_type_s_
+    extern const f_string_static_t f_fss_payload_object_type_s;
+  #endif // _di_f_fss_payload_object_type_s_
 #endif // _di_f_fss_payload_s_
 
 #ifdef __cplusplus
